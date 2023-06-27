4d72f9bb62256c28bb81e5f50e79ad18b91cea2c
gillilo <ghlwns9258@gmail.com>
2023-06-28 07:37:18 +0900

    404 Not Found! 페이지 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import Home from './Home.svelte'
 import Ex1 from './Ex1.svelte'
 import Ex2 from './Ex2.svelte'
 import Ex3 from './Ex3.svelte'
+import NotFound from './NotFound.svelte'
 
 export default {
   '/': Home,
   '/ex1': Ex1,
   '/ex2/:inpathparam': Ex2, // 라우츠 파라미터
   '/ex3': Ex3,
+  '*': NotFound, // 위 주소로 필터링이 되지 않는경우 표시되는 페이지
 }
