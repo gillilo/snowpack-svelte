35c57e110e545351dc23e9fe2e9ae669db5d8901
gillilo <ghlwns9258@gmail.com>
2023-06-28 02:06:29 +0900

    babel-plugin-transform-remove-console 추가

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,9 @@
+const production = process.env.NODE_ENV === 'production'
+function babelOptions() {
+  return {
+    plugins: production ? ['transform-remove-console'] : []
+  }
+}
 module.exports = {
   mount: {
     public: '/', // public이라는 디렉토리가 프로젝트의 루트 경로에 해당
@@ -13,8 +19,13 @@ module.exports = {
           plugins: [
             require('autoprefixer')()
           ]
-        }
+        },
+        babel: babelOptions() // svelte 컴포넌트에서 콘솔 지우는 것 (svelte 미지원 이유로... babel로 빌드한다음 적용시키는 것임)
       })
-    }]
+    }],
+    ['@snowpack/plugin-babel', {
+      transformOptions: babelOptions() // 일반적인 js에서 콘솔 지우는 것
+    }],
+    '@snowpack/plugin-dotenv'
   ]
 }
